6a202301ceffa7876064d0d207f9a963e3877119
fauzan2798 <fauzan.azhar2798@gmail.com>
2025-03-08 16:34:29 +0700

    add-navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
 import ThreeBackground from "./components/ThreeBackground";
 
 function App() {
   return (
     <>
       <ThreeBackground />
+      <Navbar />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { delay, stagger, transform } from "motion";
+import { stagger } from "motion";
 import { useAnimate } from "motion/react";
 import { useEffect, useState } from "react";
 
@@ -26,21 +26,89 @@ function Navbar() {
           ],
           [
             "li",
-            { transform: "scale(1)", opacaity: 1, filter: "blur(0px)" },
+            { transform: "scale(1)", opacity: 1, filter: "blur(0px)" },
             { delay: stagger(0.05), at: "-0.1" },
           ],
         ]
       : [
           [
             "li",
-            { transform: "scale(0.5)", opacity: 9, filter: "blur(10px)" },
-            { delay: stagger(0.05, { from: "last" }, { at: "-0.1" }) },
+            { transform: "scale(0.5)", opacity: 0, filter: "blur(10px)" },
+            { delay: stagger(0.05, { from: "last" }), at: "-0.1" },
           ],
           ["nav", { transform: "translateX(-100%)" }, { at: "-0.1" }],
         ];
-  });
 
-  return <div>Navbar</div>;
+    animate([
+      [
+        "path.top",
+        { d: isOpen ? "M 3 15.6 L 17 2.5" : "M 2 2.5 L 20 2.5" },
+        { at: "<" },
+      ],
+      ["path.middle", { opacity: isOpen ? 0 : 1 }, { at: "<" }],
+      [
+        "path.bottom",
+        { d: isOpen ? "M 3 2.5 L 17 16.346" : "M 2 16.346 L 20 16.346" },
+        { at: "<" },
+      ],
+      ...menuAnimations,
+    ]);
+  }, [isOpen, animate]);
+
+  const handleNavItemClick = (sectionId) => {
+    setIsOpen(false);
+    document
+      .getElementById(sectionId)
+      ?.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+
+  const NavItems = [
+    { id: "services", text: "What I offer?" },
+    { id: "skills", text: "My Skills" },
+    { id: "experience", text: "Education & Experience" },
+    { id: "work", text: "My Work" },
+    { id: "contact", text: "Contact" },
+  ];
+
+  return (
+    <div className="relative flex justify-between px-12 py-2">
+      <div ref={scope} className="relative">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="absolute top-4 z-40 left-4 w-12 h-12 rounded-full bg-gradient-to-br from-primary to-secondary"
+        >
+          <svg width={23} height={18} viewBox="0 0 23 18">
+            <Path d="M 2 2.5 L 20 2.5" className="top" />
+            <Path d="M 2 9.423 L 20 9.423" opacity="1" className="middle" />
+            <Path d="M 2 16.346 L 20 16.346" opacity="1" className="bottom" />
+          </svg>
+        </button>
+
+        <nav
+          className={`fixed top-0 left-0 h-full w-72 z-30 flex items-center bg-gradient-to-br from-primary to-secondary transform ${
+            isOpen ? "translate-x-0" : "-translate-x-full"
+          } transition-transform duration-300 `}
+        >
+          <ul className="flex flex-col gap-6">
+            {NavItems.map((item) => (
+              <li key={item.id} className="text-white text-4xl font-bold mt-10">
+                <a
+                  href={`#${item.id}`}
+                  onClick={() => handleNavItemClick(item.id)}
+                >
+                  {item.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      <h1 className="font-bold text-2xl text-white top-8 right-8 p-8 lg:top-12 lg:pr-16">
+        LOGO
+      </h1>
+    </div>
+  );
 }
 
 export default Navbar;
